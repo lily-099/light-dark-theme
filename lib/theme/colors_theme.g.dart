3d332fadf086746a4ac5e530e025e2b853ae94fe
Parabeac-Nest <admin@parabeac.com>
2023-02-10 10:00:06 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/colors_theme.g.dart
@@ -0,0 +1,22 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';import 'package:colors/theme/colors_text_styles.g.dart';
+import 'package:colors/theme/colors_colors.g.dart';
+
+class ColorsTheme {
+
+static const TextTheme textTheme = TextTheme(headline1: ColorsTextStyles.headline1,headline2: ColorsTextStyles.headline2,headline3: ColorsTextStyles.headline3,headline4: ColorsTextStyles.headline4,headline5: ColorsTextStyles.headline5,headline6: ColorsTextStyles.headline6,subtitle1: ColorsTextStyles.subtitle1,subtitle2: ColorsTextStyles.subtitle2,bodyText1: ColorsTextStyles.bodyText1,bodyText2: ColorsTextStyles.bodyText2,button: ColorsTextStyles.button,caption: ColorsTextStyles.caption,overline: ColorsTextStyles.overline,);
+
+static const ColorScheme light = ColorScheme.light(onPrimary: ColorsColors.lightOnPrimary,primary: ColorsColors.lightPrimary,primaryContainer: ColorsColors.lightPrimaryContainer,secondary: ColorsColors.lightSecondary,onSecondary: ColorsColors.lightOnSecondary,onPrimaryContainer: ColorsColors.lightOnPrimaryContainer,secondaryContainer: ColorsColors.lightSecondaryContainer,onSecondaryContainer: ColorsColors.lightOnSecondaryContainer,onTertiary: ColorsColors.lightOnTertiary,tertiary: ColorsColors.lightTertiary,tertiaryContainer: ColorsColors.lightTertiaryContainer,onTertiaryContainer: ColorsColors.lightOnTertiaryContainer,error: ColorsColors.lightError,onError: ColorsColors.lightOnError,errorContainer: ColorsColors.lightErrorContainer,onErrorContainer: ColorsColors.lightOnErrorContainer,background: ColorsColors.lightBackground,onBackground: ColorsColors.lightOnBackground,surface: ColorsColors.lightSurface,onSurface: ColorsColors.lightOnSurface,surfaceVariant: ColorsColors.lightSurfaceVariant,onSurfaceVariant: ColorsColors.lightOnSurfaceVariant,outline: ColorsColors.lightOutline,inverseSurface: ColorsColors.lightInverseSurface,onInverseSurface: ColorsColors.lightOnInverseSurface,inversePrimary: ColorsColors.lightInversePrimary,surfaceTint: ColorsColors.lightSurfaceTint,shadow: ColorsColors.lightShadow);
+
+static  ThemeData themeDataLight = ThemeData(textTheme: textTheme, colorScheme: light,);
+
+static const ColorScheme dark = ColorScheme.dark(primary: ColorsColors.darkPrimary,onPrimary: ColorsColors.darkOnPrimary,primaryContainer: ColorsColors.darkPrimaryContainer,onPrimaryContainer: ColorsColors.darkOnPrimaryContainer,secondary: ColorsColors.darkSecondary,onSecondary: ColorsColors.darkOnSecondary,secondaryContainer: ColorsColors.darkSecondaryContainer,onSecondaryContainer: ColorsColors.darkOnSecondaryContainer,tertiary: ColorsColors.darkTertiary,onTertiary: ColorsColors.darkOnTertiary,tertiaryContainer: ColorsColors.darkTertiaryContainer,onTertiaryContainer: ColorsColors.darkOnTertiaryContainer,error: ColorsColors.darkError,onError: ColorsColors.darkOnError,errorContainer: ColorsColors.darkErrorContainer,onErrorContainer: ColorsColors.darkOnErrorContainer,background: ColorsColors.darkBackground,onBackground: ColorsColors.darkOnBackground,surface: ColorsColors.darkSurface,onSurface: ColorsColors.darkOnSurface,surfaceVariant: ColorsColors.darkSurfaceVariant,outline: ColorsColors.darkOutline,inverseSurface: ColorsColors.darkInverseSurface,inversePrimary: ColorsColors.darkInversePrimary,surfaceTint: ColorsColors.darkSurfaceTint,onSurfaceVariant: ColorsColors.darkOnSurfaceVariant,onInverseSurface: ColorsColors.darkOnInverseSurface,shadow: ColorsColors.darkShadow);
+
+static  ThemeData themeDataDark = ThemeData(textTheme: textTheme, colorScheme: dark,);
+}
